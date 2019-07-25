907dba8c722be0d09e42e1625e2ea451b0c6b63b
david <davidwnewman91@gmail.com>
2019-07-25 09:11:05 -0500

    added points for projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,7 +9,7 @@ export default class Porfolio extends Component {
           <h1>Check Out Some of My Work.</h1>
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf" >
           {
-            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+            resumeData.portfolio && resumeData.portfolio.map((item, index)=>{
               return(
                 <div className="columns portfolio-item" id='item' >
                   <div className="item-wrap">
@@ -18,13 +18,20 @@ export default class Porfolio extends Component {
                       <div className="portfolio-item-meta">
                         <h5>{item.name}</h5>
                         <p>{item.description}</p>
+                        <ul>
+                          {item.points && item.points.map((point)=>{
+                            return(
+                              <li>{point}</li>
+                            )
+                          })}
+                        </ul>
                       </div>
                     </div>
                   </div>
                   <section className='links'>
                   </section>
-                    <p><a href={item.website}>Visit the site!</a></p>
-                    <p><a href={item.repo}>Check the Github!</a></p>
+                    <p><a href={item.website} target="_blank" rel='noopener noreferrer'>Visit the site!</a></p>
+                    <p><a href={item.repo} target="_blank" rel='noopener noreferrer'>Check the Github!</a></p>
                 </div>
               )
             })
