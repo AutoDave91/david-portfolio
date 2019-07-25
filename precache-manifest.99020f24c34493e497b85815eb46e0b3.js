db71c433b39659f4c5bd04e9cea95cc75df68392
david <davidwnewman91@gmail.com>
2019-07-25 15:31:35 -0500

    Updates

---
+++ precache-manifest.99020f24c34493e497b85815eb46e0b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c96af9e5e784a278eb2d",
+    "url": "/static/css/main.598c648f.chunk.css"
+  },
+  {
+    "revision": "c96af9e5e784a278eb2d",
+    "url": "/static/js/main.c96af9e5.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "85fa93f4e782bb43bc4ceeb74e1cfa9c",
+    "url": "/index.html"
+  }
+];
