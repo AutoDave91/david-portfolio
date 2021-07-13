9e683180f3c29c591167ff76dd42264c339f9435
david <davidwnewman91@gmail.com>
2021-07-13 14:33:20 -0500

    July 13 update

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
 
             <div className="three columns">
 
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+               <img className="profile-pic"  src="images/headshot-min.jpeg" alt="headshot of David" />
 
             </div>
 
