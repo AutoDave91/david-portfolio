97ed545d454f85561c972064eef440da2b3d3c6d
david <davidwnewman91@gmail.com>
2022-04-13 09:39:40 -0500

    headshot update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -47,33 +47,6 @@ export default  class Resume extends Component {
           </div>
 
         </div>
-
-         <div className="row education">
-
-            <div className="three columns header-col">
-               <h1><span>Education</span></h1>
-            </div>
-
-            <div className="nine columns main-col">
-              {
-                resumeData.education && resumeData.education.map((item)=>{
-                  return(
-                    <div className="row item">
-                       <div className="twelve columns">
-                          <h3>{item.UniversityName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
-                    </div>
-                  )
-                })
-              }
-            </div>
-         </div>
         <div className="row work">
             <div className="three columns header-col">
                <h1><span>Work</span></h1>
@@ -100,6 +73,33 @@ export default  class Resume extends Component {
                 })
               }
             </div> 
+         </div>
+
+         <div className="row education">
+
+            <div className="three columns header-col">
+               <h1><span>Education</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.education && resumeData.education.map((item)=>{
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.UniversityName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
+                          <p>
+                          {item.Achievements}
+                          </p>
+                       </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
          </div>
 
 
